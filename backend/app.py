96e4cebab97a57b0e1ec7f08a233f3e8e74141f9
gaboToro <104457588+gaboToro@users.noreply.github.com>
2025-12-13 14:30:50 -0500

    feat: update backend application and HTML template

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates') # Indicamos a Flask dónde buscar templates
@@ -7,7 +8,8 @@ def home():
     # Renderiza el archivo index.html.
     # Puedes pasar variables aquí si quieres, por ejemplo:
     # return render_template('index.html', version='1.0')
-    return render_template('index.html')
+    return render_template('index.html', image_tag=os.getenv('IMAGE_TAG', 'dev'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    debug_mode = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+    app.run(host='0.0.0.0', port=80)
